caff9a45db01dd6070ef13f5a820dba747fd2a0d
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-13 14:42:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 array = [9, 10, 16, 25]
 
 def square_array(array)
@@ -6,8 +7,35 @@ def square_array(array)
   squared = []
   
   array.each {|number| squared << number * number}
+=======
+#array = [1, 2, 3]
+
+#def square_array(array)
+  
+  #squared = []
+  
+  #array.each {|number| squared << number * number}
+  
+  #puts squared
+ # return squared
+  
+#end
+
+big_array = [9, 10, 16, 25]
+
+def big_square_array(big_array)
+  
+  squared = []
+  
+  big_array.each {|number| squared << number * number}
+>>>>>>> 0c619e9f55153e48a88fbd70142986499783c921
   
   puts squared
   return squared
   
+<<<<<<< HEAD
 end
+=======
+end
+
+>>>>>>> 0c619e9f55153e48a88fbd70142986499783c921
